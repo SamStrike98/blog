96a3e3c721d6b4fc734697791c381618865ae8b0
Sam Strike <sam.strike1@gmail.com>
2024-06-25 13:47:15 +0100

    Handle profile for non signed in users.

--- app/profile/[id]/page.js
+++ app/profile/[id]/page.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import { auth } from '@/auth'
+import { redirect } from 'next/navigation';
 
 const page = async ({ params }) => {
     const id = params.id
     const session = await auth()
-    return (
-        <div>
-            {id} - {id === session?.user.id ? <p>Can edit</p> : <p>Cannot edit</p>}
-        </div>
-    )
+    if (!session?.user) {
+        redirect('/')
+    } else {
+        return (
+            <div>
+                {id} - {id === session?.user.id ? <p>Can edit</p> : <p>Cannot edit</p>}
+            </div>
+        )
+    }
+
 }
 
 export default page
